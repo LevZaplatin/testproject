77b1a39e35efb0b9fdf6b91c1440d3563b8bf0d7
Alexandr <33527612+AYaro@users.noreply.github.com>
2018-03-09 22:38:50 +0300

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
     path('admin/', admin.site.urls),
-    path('^/', include('mainApp.urls'))
+    path('^$', include('mainApp.urls'))
 ]
