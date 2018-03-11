ac05132f61504557250ee4a849fa670ed08974e9
Alexandr <33527612+AYaro@users.noreply.github.com>
2018-03-11 17:11:37 +0300

    Update views.py

--- news/views.py
+++ news/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import article
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>This will be a list of news articles</h1>")
+    all_articles = article.Objects.All()
+    html = ''
+    for article in all_articles:
+        url = '/news/' + srt(article.id) + '/'
+        html += '<a href="' + url + '">' + article.article_title + '<a></br>'
+    return HttpResponse(http)
 
 def detail(request, article_id):
     return HttpResponse("<h1>This will be a detailed of article id: " + str(article_id) + "</h1>" )
