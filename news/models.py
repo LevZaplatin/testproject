8da5ffd2542394d0d44e23f9f4bdb6a3e0adbe62
Alexandr <33527612+AYaro@users.noreply.github.com>
2018-03-11 15:56:59 +0300

    added author

--- news/models.py
+++ news/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    author = models.CharField(max_length=200)
     
 # Create your models here.
