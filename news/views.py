5966fe79fcbb97bad571961602130e1c9ade14df
Alexandr <33527612+AYaro@users.noreply.github.com>
2018-03-11 16:22:09 +0300

    Update views.py

--- news/views.py
+++ news/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return None
+def index(request):
+    return HttpResponse("<h1>This will be a list of news articles</h1>")
+
+def detail(request, article_id)
+    return HttpResponse("<h1>This will be a detailed of article id: " + str(article_id) + "</h1>" )
+
+    
